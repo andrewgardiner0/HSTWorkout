cb75d1a90a51da1bec644677d635ac05e1b1abd1
andrewgardiner0 <andrewgardiner0@gmail.com>
2016-11-20 19:40:10 +0000

    Work on calculate class, splits up excercises and works out weights for each rep range.

--- app/src/main/java/com/hst/andrew/hstworkout/Calculate.java
+++ app/src/main/java/com/hst/andrew/hstworkout/Calculate.java
@@ -7,21 +7,59 @@ package com.hst.andrew.hstworkout;
 public class Calculate {
 
 
-
+    //Todo : split values from text boxes and sort into arrays for calculation -- done
+    //Todo : calculate the 5 previous workout values  for the cycle for each rep range -- done
     public int[] work(int[] val){
-        int[] ex1 = new int[2];
-        int[] ex2 = new int[2];
-        int[] ex3 = new int[2];
-        int[] ex4 = new int[2];
-        int[] ex5 = new int[2];
-        int[] ex6 = new int[2];
-        int[] ex7 = new int[2];
-        int[] ex8 = new int[2];
-        int[] ex9 = new int[2];
+        int[] ex1 = new int[3];
+        int[] ex2 = new int[3];
+        int[] ex3 = new int[3];
+        int[] ex4 = new int[3];
+        int[] ex5 = new int[3];
+        int[] ex6 = new int[3];
+        int[] ex7 = new int[3];
+        int[] ex8 = new int[3];
+        int[] ex9 = new int[3];
 
         int[] fin = val;
         int flag = 0;
-      for(int i=0;i<=26;i++){
+      for(int i=0;i<=val.length-1;i++){
+          if(flag ==3)
+              flag =0;
+        if(i<=3){
+            ex1[flag] = val[i];
+        }
+          else if(i>3 && i <= 7){
+            ex2[flag] = val[i];
+
+        }
+        else if(i>7 && i <= 11){
+            ex3[flag] = val[i];
+
+        }
+        else if(i>11 && i <= 15){
+            ex4[flag] = val[i];
+
+        }
+        else if(i>15 && i <= 19){
+            ex5[flag] = val[i];
+
+        }
+        else if(i>19 && i <= 23){
+            ex6[flag] = val[i];
+
+        }
+        else if(i>23 && i <= 27){
+            ex7[flag] = val[i];
+
+        }
+        else if(i>27 && i <= 31){
+            ex8[flag] = val[i];
+
+        }
+        else if(i>31 && i <= 35){
+            ex9[flag] = val[i];
+
+        }
 
 
       }
@@ -29,4 +67,43 @@ public class Calculate {
         return fin;
 
     }
+
+    public int minus(int a, int decrement){
+        return a - decrement;
+    }
+
+    public int[] reps(int [] ex){
+        //int index = ex.length -1;
+        int decrement =ex[ex.length-1];
+       int[] fifteenrep = new int[5];
+        int[] tenrep = new int[5];
+        int[] fiverep = new int[5];
+
+        int currentfiteen = ex[0], currentten=ex[1],currentfive =ex[2];
+        for(int i=4;i>=0;i--){
+
+            fifteenrep[i] = minus(currentfiteen, decrement);
+            tenrep[i] = minus(currentten, decrement);
+            fiverep[i] = minus(currentfive, decrement);
+
+            currentfiteen = minus(currentfiteen,decrement);
+            currentten = minus(currentten, decrement);
+            currentfive = minus(currentfive, decrement);
+        }
+        fifteenrep[5] = ex[0];
+        tenrep[5] = ex[1];
+        fiverep[5] = ex[2];
+        int[] finalreps = new int[fifteenrep.length + tenrep.length + fiverep.length];
+
+        //kinda dodgy
+        System.arraycopy(fifteenrep,0,finalreps,0,fifteenrep.length);
+        System.arraycopy(tenrep,0,finalreps,fifteenrep.length, tenrep.length);
+        System.arraycopy(fiverep,0,finalreps,fifteenrep.length + tenrep.length, fiverep.length);
+
+
+        return finalreps;
+    }
+
+
+
 }
--- app/src/main/java/com/hst/andrew/hstworkout/WeightCalculations.java
+++ app/src/main/java/com/hst/andrew/hstworkout/WeightCalculations.java
@@ -14,7 +14,9 @@ public class WeightCalculations extends AppCompatActivity {
     EditText
             ex1_1,ex2_1,ex3_1,ex4_1,ex5_1,ex6_1,ex7_1,ex8_1,ex9_1,
             ex1_2,ex2_2,ex3_2,ex4_2,ex5_2,ex6_2,ex7_2,ex8_2,ex9_2,
-            ex331,ex2_3,ex3_3,ex4_3,ex5_3,ex6_3,ex7_3,ex8_3,ex9_3, decrement;
+            ex1_3,ex2_3,ex3_3,ex4_3,ex5_3,ex6_3,ex7_3,ex8_3,ex9_3,
+            decrement1, decrement2,decrement3,decrement4,decrement5,
+            decrement6,decrement7,decrement8,decrement9;
 
 
     TextView  ex1,ex2,ex3,ex4,ex5,ex6,ex7,ex8,ex9;
@@ -57,7 +59,7 @@ public class WeightCalculations extends AppCompatActivity {
         ex7_2 = (EditText) findViewById(R.id.ex7_2);
         ex8_2 = (EditText) findViewById(R.id.ex8_2);
         ex9_2 = (EditText) findViewById(R.id.ex9_2);
-        ex331 = (EditText) findViewById(R.id.ex1_3);
+        ex1_3 = (EditText) findViewById(R.id.ex1_3);
         ex2_3 = (EditText) findViewById(R.id.ex2_3);
         ex3_3 = (EditText) findViewById(R.id.ex3_3);
         ex4_3 = (EditText) findViewById(R.id.ex4_3);
@@ -66,41 +68,70 @@ public class WeightCalculations extends AppCompatActivity {
         ex7_3 = (EditText) findViewById(R.id.ex7_3);
         ex8_3 = (EditText) findViewById(R.id.ex8_3);
         ex9_3 = (EditText) findViewById(R.id.ex9_3);
-        decrement = (EditText) findViewById(R.id.decrementval);
+        decrement1 = (EditText) findViewById(R.id.decrementval1);
+        decrement2 = (EditText) findViewById(R.id.decrementval2);
+        decrement3 = (EditText) findViewById(R.id.decrementval3);
+        decrement4 = (EditText) findViewById(R.id.decrementval4);
+        decrement5 = (EditText) findViewById(R.id.decrementval5);
+        decrement6 = (EditText) findViewById(R.id.decrementval6);
+        decrement7 = (EditText) findViewById(R.id.decrementval7);
+        decrement8 = (EditText) findViewById(R.id.decrementval8);
+        decrement9 = (EditText) findViewById(R.id.decrementval9);
 
 
     }
 
     public int[] values(int x){
+        //All values into val array.
+        //nine rows, three columns.
+
         int[] val = new int[x];
         val[0] = Integer.parseInt(ex1_1.getText().toString());
         val[1] = Integer.parseInt(ex1_2.getText().toString());
-        val[2] = Integer.parseInt(ex331.getText().toString());
-        val[3] = Integer.parseInt(ex2_1.getText().toString());
-        val[4] = Integer.parseInt(ex2_2.getText().toString());
-        val[5] = Integer.parseInt(ex2_3.getText().toString());
-        val[6] = Integer.parseInt(ex3_1.getText().toString());
-        val[7] = Integer.parseInt(ex3_2.getText().toString());
-        val[8] = Integer.parseInt(ex3_3.getText().toString());
-        val[9] = Integer.parseInt(ex4_1.getText().toString());
-        val[10] = Integer.parseInt(ex4_2.getText().toString());
-        val[11] = Integer.parseInt(ex4_3.getText().toString());
-        val[12] = Integer.parseInt(ex5_1.getText().toString());
-        val[13] = Integer.parseInt(ex5_2.getText().toString());
-        val[14] = Integer.parseInt(ex5_3.getText().toString());
-        val[15] = Integer.parseInt(ex6_1.getText().toString());
-        val[16] = Integer.parseInt(ex6_2.getText().toString());
-        val[17] = Integer.parseInt(ex6_3.getText().toString());
-        val[18] = Integer.parseInt(ex7_1.getText().toString());
-        val[19] = Integer.parseInt(ex7_2.getText().toString());
-        val[20] = Integer.parseInt(ex7_3.getText().toString());
-        val[21] = Integer.parseInt(ex8_1.getText().toString());
-        val[22] = Integer.parseInt(ex8_2.getText().toString());
-        val[23] = Integer.parseInt(ex8_3.getText().toString());
-        val[24] = Integer.parseInt(ex9_1.getText().toString());
-        val[25] = Integer.parseInt(ex9_2.getText().toString());
-        val[26] = Integer.parseInt(ex9_3.getText().toString());
-        val[27] = Integer.parseInt(decrement.getText().toString());
+        val[2] = Integer.parseInt(ex1_3.getText().toString());
+        val[3] = Integer.parseInt(decrement1.getText().toString());
+        val[4] = Integer.parseInt(ex2_1.getText().toString());
+        val[5] = Integer.parseInt(ex2_2.getText().toString());
+        val[6] = Integer.parseInt(ex2_3.getText().toString());
+        val[7] = Integer.parseInt(decrement2.getText().toString());
+        val[8] = Integer.parseInt(ex3_1.getText().toString());
+        val[9] = Integer.parseInt(ex3_2.getText().toString());
+        val[10] = Integer.parseInt(ex3_3.getText().toString());
+        val[11] = Integer.parseInt(decrement3.getText().toString());
+        val[12] = Integer.parseInt(ex4_1.getText().toString());
+        val[13] = Integer.parseInt(ex4_2.getText().toString());
+        val[14] = Integer.parseInt(ex4_3.getText().toString());
+        val[15] = Integer.parseInt(decrement4.getText().toString());
+        val[16] = Integer.parseInt(ex5_1.getText().toString());
+        val[17] = Integer.parseInt(ex5_2.getText().toString());
+        val[18] = Integer.parseInt(ex5_3.getText().toString());
+        val[19] = Integer.parseInt(decrement5.getText().toString());
+        val[20] = Integer.parseInt(ex6_1.getText().toString());
+        val[21] = Integer.parseInt(ex6_2.getText().toString());
+        val[22] = Integer.parseInt(ex6_3.getText().toString());
+        val[23] = Integer.parseInt(decrement6.getText().toString());
+        val[24] = Integer.parseInt(ex7_1.getText().toString());
+        val[25] = Integer.parseInt(ex7_2.getText().toString());
+        val[26] = Integer.parseInt(ex7_3.getText().toString());
+        val[27] = Integer.parseInt(decrement7.getText().toString());
+        val[28] = Integer.parseInt(ex8_1.getText().toString());
+        val[29] = Integer.parseInt(ex8_2.getText().toString());
+        val[30] = Integer.parseInt(ex8_3.getText().toString());
+        val[31] = Integer.parseInt(decrement8.getText().toString());
+        val[32] = Integer.parseInt(ex9_1.getText().toString());
+        val[33] = Integer.parseInt(ex9_2.getText().toString());
+        val[34] = Integer.parseInt(ex9_3.getText().toString());
+        val[35] = Integer.parseInt(decrement9.getText().toString());
+
+
+
+
+
+
+
+
+
+
 
 
         return val;
